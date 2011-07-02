3a07978a483a1b3fb52e13f9ef157e3e912740bd
Sebastian Johnsson <sebastian@agiley.se>
2011-07-02 17:34:51 +0200

    Updates

--- lib/search_utilities.rb
+++ lib/search_utilities.rb
@@ -1,8 +1,8 @@
 module SearchUtilities
-   require File.expand_path("search_utilities/railtie", __FILE__) if defined?(Rails)
-   require File.expand_path("search_utilities/array_utility", __FILE__)
-   require File.expand_path("search_utilities/controller_utility", __FILE__)
-   require File.expand_path("search_utilities/cookies", __FILE__)
-   require File.expand_path("search_utilities/search", __FILE__)
-   require File.expand_path("search_utilities/search_helper", __FILE__)
+   require File.expand_path("../search_utilities/railtie", __FILE__) if defined?(Rails)
+   require File.expand_path("../search_utilities/array_utility", __FILE__)
+   require File.expand_path("../search_utilities/controller_utility", __FILE__)
+   require File.expand_path("../search_utilities/cookies", __FILE__)
+   require File.expand_path("../search_utilities/search", __FILE__)
+   require File.expand_path("../search_utilities/search_helper", __FILE__)
 end
